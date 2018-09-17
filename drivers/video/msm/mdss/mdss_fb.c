e2976a624daca54d379255c1649c081333246384
bitrvmpd <e.noyer.silva@gmail.com>
2018-09-17 23:56:56 +0000

    :bug: Fixed random reboots
    - Experimental eas cpu config.
    - Reverted xiaomi's way to set brightness, was annoying.
    - Fixed audio.
    
     Conflicts:
    	drivers/video/msm/mdss/mdss_fb.c

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -292,7 +292,7 @@ static void mdss_fb_set_bl_brightness(struct led_classdev *led_cdev,
 {
 	struct msm_fb_data_type *mfd = dev_get_drvdata(led_cdev->dev->parent);
 	int bl_lvl;
-		
+
 	if (mfd->boot_notification_led) {
 		led_trigger_event(mfd->boot_notification_led, 0);
 		mfd->boot_notification_led = NULL;
